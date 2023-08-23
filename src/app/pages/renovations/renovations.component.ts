9b2bb3fee05ab0fc40ec681fa4ef0b2592064cbf
devaccountdn <devaccdn.99@gmail.com>
2023-08-23 17:58:32 +0530

    italic font removed and carousel added for home page

--- src/app/pages/renovations/renovations.component.ts
+++ src/app/pages/renovations/renovations.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./renovations.component.scss']
 })
 export class RenovationsComponent {
+  ngAfterViewInit(): void {
+    this.startAutoplay();
+  }
 
+  startAutoplay(): void {
+    const carousel = document.querySelector('.carousel');
+    const interval = 3000; // Change this to adjust the interval
+
+    setInterval(() => {
+      if (carousel) {
+        const activeSlide = carousel.querySelector('.carousel-item.active');
+        const nextSlide = activeSlide?.nextElementSibling || carousel.querySelector('.carousel-item:first-child');
+        activeSlide?.classList.remove('active');
+        nextSlide?.classList.add('active');
+      }
+    }, interval);
+  }
 }
