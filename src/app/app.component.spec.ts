658bf52a4149810b70658c25c0b9320ec520fdea
devaccountdn <devaccdn.99@gmail.com>
2023-08-24 19:17:36 +0530

    Final design review

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'saldon'`, () => {
+  it(`should have as title 'Gold Leaf Construction'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('saldon');
+    expect(app.title).toEqual('Gold Leaf Construction');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('saldon app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Gold Leaf Construction app is running!');
   });
 });
